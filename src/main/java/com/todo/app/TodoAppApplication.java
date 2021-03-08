8f21250cd03c399c37a9ed492d911783435aa117
Smita Bhosale <smitabhosale9990@gmail.com>
2021-03-08 14:58:57 +0530

    configured properties in application properties added mysql dependancy in build.gradle added packages in component scan Added new controller

--- src/main/java/com/todo/app/TodoAppApplication.java
+++ src/main/java/com/todo/app/TodoAppApplication.java
@@ -2,12 +2,13 @@ package com.todo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.todo.controller","com.todo.service"})
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
-
 }
